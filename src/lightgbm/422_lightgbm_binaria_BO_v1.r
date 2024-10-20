bb4c32b3111515e71de79d210dfa1dea9256eae2
Gaston Daniel Festa <gdfesta@gmail.com>
2024-10-20 11:36:04 -0300

    otra prueba

--- src/lightgbm/422_lightgbm_binaria_BO_v1.r
+++ src/lightgbm/422_lightgbm_binaria_BO_v1.r
@@ -33,7 +33,7 @@ options(error = function() {
 #  muy pronto esto se leera desde un archivo formato .yaml
 PARAM <- list()
 
-PARAM$experimento <- "HT4220"
+PARAM$experimento <- "HT4220_6"
 
 PARAM$input$training <- c(202107) # los meses en los que vamos a entrenar
 
@@ -53,13 +53,15 @@ hs <- makeParamSet(
   makeNumericParam("learning_rate", lower = 0.01, upper = 0.3),
   makeIntegerParam("n_estimators", lower = 10L, upper = 2000L),
   makeIntegerParam("num_leaves", lower = 8L, upper = 4096L),
-  makeIntegerParam("max_depth", lower = 3L, upper = 12L),
+  makeIntegerParam("max_depth", lower = 3L, upper = 15L),
   makeIntegerParam("min_gain_to_split", lower = 0L, upper = 15L),
-  makeIntegerParam("min_data_in_leaf", lower = 200L, upper = 12000L),
+  makeIntegerParam("min_data_in_leaf", lower = 100L, upper = 12000L),
   makeNumericParam("feature_fraction", lower = 0.1, upper = 1.0),
   makeNumericParam("bagging_fraction", lower = 0.2, upper = 0.95),
-  makeIntegerParam("bagging_freq", lower = 0L, upper = 1L),
-  makeIntegerParam("envios", lower = 5000L, upper = 15000L)
+  makeIntegerParam("bagging_freq", lower = 0L, upper = 10L),
+  makeIntegerParam("lambda_l1", lower = 0L, upper = 100),
+  makeIntegerParam("lambda_l2", lower = 0L, upper = 10),
+  makeIntegerParam("envios", lower = 5000L, upper = 30000L)
 )
 
 #------------------------------------------------------------------------------
